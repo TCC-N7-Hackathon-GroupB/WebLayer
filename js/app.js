573d936a65d56374b2f812c1ddd9eb01c4494092
Fumiyo Conway <fumiyo.conway@tcc-fconway.local>
2014-10-30 10:01:26 -0700

    Initial check in for side dressing app

---
+++ js/app.js
@@ -0,0 +1,153 @@
+/* jshint strict: true */
+
+(function() {
+	
+	"use strict";
+
+
+	var Graph = function() {
+
+		var width = document.getElementById("d3nitrogenGraph").offsetWidth,
+			aspect = 1/3,
+			height = width * aspect,
+			margin = {top: 50, right: 30, bottom: 40, left: 50},
+			containerWidth = width - margin.right - margin.left,
+			containerHeight = height - margin.top - margin.bottom,
+			xAxis, 
+			yAxis, 
+			scaleX, 
+			scaleY, 
+			line, 
+			tip,
+			nitroData = data2,
+			cropData = data;
+
+
+		this.init = function() {
+			document.addEventListener("DOMContentLoaded", this.scaffold(this));
+		};
+
+		this.scaffold = function(me) {
+			me.formatData(nitroData);
+			me.formatData(cropData)
+			me.setGraphParameters();
+			me.render();
+		};
+
+		this.formatData = function(thisData) {
+			var parseDate = d3.time.format("%d-%b-%y").parse;
+
+			thisData.forEach( function(d) {
+				d.date = parseDate(d.date);
+				d.close = d.close;
+			});
+		};
+
+		this.setGraphParameters = function() {
+
+			scaleX =  d3.time.scale()
+				.range([0, containerWidth - 250])
+				.domain( d3.extent(nitroData, function(d) { return d.date; }) );
+
+			scaleY = d3.scale.linear()
+				.range([containerHeight, 0])
+				.domain([0, d3.max(nitroData, function(d) { return d.close; }) ]);
+
+			xAxis = d3.svg.axis()
+				.scale(scaleX)
+				.orient("bottom")
+				.tickFormat(d3.time.format("%b %e"));
+
+			yAxis = d3.svg.axis()
+				.scale(scaleY)
+				.orient("left")
+				.ticks(6);
+
+			line = d3.svg.line()
+				.x( function(d) { return scaleX(d.date); })
+				.y( function(d) { return scaleY(d.close); });
+
+			tip = d3.tip()
+				.attr("class", "tip")
+				.offset([-50, 0])
+				.direction("s")
+				.html(function(d) {
+
+					var type = this.classList.contains("nitrogen");
+					var tipTitle, 
+					  tipDateFormat = d3.time.format("%e %b, %Y"), 
+					  tipDate = tipDateFormat(d.date);
+
+					if (type === true) {
+					  tipTitle = "Pro available Nitrogen";
+					}
+					else {
+					  tipTitle = "Crop Uptake";
+					}
+
+					return tipTitle + " of " + d.close + " lbs/ac on " + tipDate;
+				});
+		};
+
+		this.render = function() {
+			this.createContainer();
+			this.renderGraph(nitroData, "nitrogen");
+			this.renderGraph(cropData, "hungryPlants");
+			this.renderLegends();
+		};
+
+		this.createContainer = function() {
+			var svg = d3.select("svg")
+				.attr("width", "100%")
+				.attr("height", "100%")
+				.attr('viewBox','0 0 '+Math.min(window.innerWidth, window.innerHeight)+' '+Math.min(window.innerWidth, window.innerHeight))
+    		.attr('preserveAspectRatio','xMinYMin')
+			.append("g")
+				.attr("transform", "translate(" + margin.left + "," + margin.top + ")")
+				.call(tip);
+
+			this.svg = svg;
+		};
+
+		this.renderGraph = function(data, keyword) {
+			var graphContainer = this.svg.append("g")
+				.attr("width", containerWidth)
+				.attr("height", containerHeight);
+
+			graphContainer.append("path")
+			.datum(data)
+			.attr("d", line)
+			.classed("line line--" + keyword, true);
+
+			graphContainer.selectAll("circle")
+			.data(data)
+			.enter().append("circle")
+			.classed("point--" + keyword, true)
+			.attr("r", 5)
+			.attr("cx", function(d) { return scaleX(d.date);})
+			.attr("cy", function(d) { return scaleY(d.close); })
+			.on("mouseover", tip.show)
+			.on("mouseout", tip.hide);
+		};
+
+		this.renderLegends = function() {
+			this.svg.append("g")
+				.call(xAxis)
+				.classed("axis", true)
+				.attr("transform", "translate(0," + containerHeight + ")");
+
+			this.svg.append("g")
+				.call(yAxis)
+				.classed("axis", true)
+				.append("text")
+				.attr("x", -40)
+				.attr("y", -20)
+				.text("Nitrogen (lbs N/ac)");
+		};
+	};
+
+
+	var nitrogen = new Graph();
+	nitrogen.init();
+        
+}());
---
+++ js/data.js
@@ -0,0 +1,95 @@
+var data = [
+{
+    "date": "31-Nov-14",
+    "close": 72.13
+}, {
+    "date": "31-Oct-14",
+    "close": 72.13
+}, {
+    "date": "9-Sep-14",
+    "close": 72.13
+}, {
+    "date": "27-Aug-14",
+    "close": 72.10
+}, {
+    "date": "17-Jul-14",
+    "close": 70.12
+}, {
+    "date": "6-Jun-14",
+    "close": 63.97
+}, {
+    "date": "25-May-14",
+    "close": 59.66
+}, {
+    "date": "20-Apr-14",
+    "close": 0
+}, {
+    "date": "9-Mar-14",
+    "close": 0
+}, {
+    "date": "6-Feb-14",
+    "close": 0
+}, {
+    "date": "5-Jan-14",
+    "close": 0
+}, {
+    "date": "5-Dec-13",
+    "close": 0
+}, {
+    "date": "5-Nov-13",
+    "close": 0
+}, {
+    "date": "5-Oct-13",
+    "close": 0
+}, {
+    "date": "5-Sep-13",
+    "close": 0
+}];
+
+var data2 = [
+{
+    "date": "31-Nov-14",
+    "close": 12.13
+}, {
+    "date": "31-Oct-14",
+    "close": 18.13
+}, {
+    "date": "9-Sep-14",
+    "close": 12.13
+}, {
+    "date": "27-Aug-14",
+    "close": 12.10
+}, {
+    "date": "17-Jul-14",
+    "close": 20.12
+}, {
+    "date": "6-Jun-14",
+    "close": 33.97
+}, {
+    "date": "25-May-14",
+    "close": 59.66
+}, {
+    "date": "20-Apr-14",
+    "close": 79.19
+}, {
+    "date": "9-Mar-14",
+    "close": 96.44
+}, {
+    "date": "6-Feb-14",
+    "close": 98.56
+}, {
+    "date": "5-Jan-14",
+    "close": 110.43
+}, {
+    "date": "5-Dec-13",
+    "close": 115.76
+}, {
+    "date": "5-Nov-13",
+    "close": 120
+}, {
+    "date": "5-Oct-13",
+    "close": 10.23
+}, {
+    "date": "5-Sep-13",
+    "close": 0
+}];
